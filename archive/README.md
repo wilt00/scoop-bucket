82f3154c38ec133e4c96ba00e38c8b4b70a3b9c6
Test User <will.toher.0@gmail.com>
2025-01-12 08:47:51 -0800

    archive nonfunctioning manifests

---
+++ archive/README.md
@@ -0,0 +1,1 @@
+The manifests in this directory either do not work or are unmaintained, and are kept here purely for future reference.
---
+++ archive/cosmosrc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export PATH="$HOME/scoop/apps/cosmos/current/bin:$HOME/scoop/apps/cosmocc/current/bin"
+echo "Loaded init file"
